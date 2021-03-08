7df4e2d5aa62148d8ed73341cd251c0e46bae698
t-kubrak <chikashikaari@gmail.com>
2021-03-08 15:47:45 +0200

    feat: show items in deck list, initial

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,19 +1,60 @@
-import React from 'react';
-import {StyleSheet, Text, View} from "react-native";
+import React, {useState} from 'react';
+import {StyleSheet, FlatList, Text, StatusBar, View, TouchableOpacity} from "react-native";
+
+const DATA = [
+    {
+        id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+        title: "First Item",
+    },
+    {
+        id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+        title: "Second Item",
+    }
+];
+
+const Item = ({ item, onPress, style }) => (
+    <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+        <Text style={styles.title}>{item.title}</Text>
+    </TouchableOpacity>
+);
 
 export default function DeckList() {
+    const [selectedId, setSelectedId] = useState(null);
+
+    const renderItem = ({ item }) => {
+        const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+
+        return (
+            <Item
+                item={item}
+                onPress={() => setSelectedId(item.id)}
+                style={{ backgroundColor }}
+            />
+        );
+    };
+
+
     return (
-        <View style={styles.container}>
-            <Text>DeckList</Text>
-        </View>
+        <FlatList
+            data={DATA}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            extraData={selectedId}
+        />
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
     },
 });
