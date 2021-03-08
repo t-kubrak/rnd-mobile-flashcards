0c29945b2a9e48ee6801c2036239cf53f3a2dacd
t-kubrak <chikashikaari@gmail.com>
2021-03-08 21:27:14 +0200

    feat: add initial data

---
+++ utils/data.js
@@ -0,0 +1,32 @@
+let decks = {
+    React: {
+        id: 'React',
+        title: 'React',
+        questions: [
+            {
+                question: 'What is React?',
+                answer: 'A library for managing user interfaces'
+            },
+            {
+                question: 'Where do you make Ajax requests in React?',
+                answer: 'The componentDidMount lifecycle event'
+            }
+        ]
+    },
+    JavaScript: {
+        id: 'JavaScript',
+        title: 'JavaScript',
+        questions: [
+            {
+                question: 'What is a closure?',
+                answer: 'The combination of a function and the lexical environment within which that function was declared.'
+            }
+        ]
+    }
+}
+
+export function getDecks () {
+    return new Promise((res, rej) => {
+        setTimeout(() => res({...decks}), 1000)
+    })
+}
