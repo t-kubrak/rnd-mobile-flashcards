94ccdd85f9686e5b67ee10cb1681cb33a5010fcb
t-kubrak <taras.m.kubrak@gmail.com>
2021-03-10 12:45:27 +0200

    feat: quiz handle answers and show the result

--- components/Quiz.js
+++ components/Quiz.js
@@ -2,41 +2,66 @@ import React from 'react';
 import {StyleSheet, Text, View, Button, Pressable} from "react-native";
 import {connect} from "react-redux";
 
-function Quiz({deck, dispatch, navigation}) {
-    const handleChoice = () => {
+class Quiz extends React.Component {
+    state = {
+        currentQuestionId: 0,
+        correctAnswersCount: 0
+    }
+
+    handleChoice = (isCorrect) => {
+        const {currentQuestionId, correctAnswersCount} = this.state
+
+        this.setState({
+            correctAnswersCount: isCorrect ? correctAnswersCount + 1 : correctAnswersCount,
+            currentQuestionId: currentQuestionId + 1
+        })
+    }
+
+    handleQuestionAnswer = () => {
 
     }
 
-    const handleQuestionAnswer = () => {
+    render() {
+        const {deck} = this.props
+        const {currentQuestionId, correctAnswersCount} = this.state
+        const lastQuestionId = deck.questions.length - 1
 
-    }
-
-    return (
-        <View style={styles.containerOuter}>
-            <Text style={styles.questionCounter}>1/{deck.questions.length}</Text>
-            <View style={styles.container}>
-                <Text style={styles.title}>{deck.questions[0].question}</Text>
-                <Pressable onPress={handleQuestionAnswer}>
-                    <Text>Answer</Text>
-                </Pressable>
-
-                <View style={{marginTop: 30}}>
-                    <Button
-                        onPress={handleChoice}
-                        title="Correct"
-                        color='green'
-                    />
+        if (currentQuestionId > lastQuestionId) {
+            return (
+                <View style={styles.container}>
+                    <Text style={styles.title}>Your score: {Math.floor(correctAnswersCount/deck.questions.length*100)}%</Text>
                 </View>
-                <View style={{marginTop: 30}}>
-                    <Button
-                        onPress={handleChoice}
-                        title="Incorrect"
-                        color='red'
-                    />
+            )
+        }
+
+        return (
+            <View style={styles.containerOuter}>
+                <Text style={styles.questionCounter}>{currentQuestionId+1}/{deck.questions.length}</Text>
+                <View style={styles.container}>
+                    <Text style={styles.title}>{deck.questions[currentQuestionId].question}</Text>
+                    <Pressable onPress={this.handleQuestionAnswer}>
+                        <Text>Answer</Text>
+                    </Pressable>
+
+                    <View style={{marginTop: 30}}>
+                        <Button
+                            onPress={() => this.handleChoice(true)}
+                            title="Correct"
+                            color='green'
+                            value={true}
+                        />
+                    </View>
+                    <View style={{marginTop: 30}}>
+                        <Button
+                            onPress={() => this.handleChoice(false)}
+                            title="Incorrect"
+                            color='red'
+                        />
+                    </View>
                 </View>
             </View>
-        </View>
-    );
+        );
+    }
 }
 
 function mapStateToProps({decks}, {route}) {
